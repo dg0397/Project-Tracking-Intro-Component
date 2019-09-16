24f74cf5a3c7f9d7baca711e6d8ff16b8ac780bd
dg0397 <rgg0397@gmail.com>
2019-09-15 22:42:12 -0400

    first commit

---
+++ src/app/images.js
@@ -0,0 +1,5 @@
+export function importImages(){
+    require("../static/images/favicon-32x32.png");
+    require("../static/images/illustration-devices.svg");
+    require("../static/images/logo.svg");
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "./styles.scss";
+import {importImages} from "./app/images";
+
+
+importImages();
